68d467f30b196898508c8bc64c61ae2db44c7a9b
Rhameez13 <rhameez13@gmail.com>
2025-06-17 15:35:17 +0200

    rhameez and khotso changes

--- sendmail.php
+++ sendmail.php
@@ -3,7 +3,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $to = "support@kmtelecomsa.co.za";
     $subject = $_POST["subject"];
     $message = $_POST["message"];
-    $headers = "smtp.kmtelecomsa.co.za: " . $_POST["info@kmtelecoms.co.za"];
+    $headers = "From: " . $_POST["email"];
     mail($to, $subject, $message, $headers);
     echo "Message sent!";
 }
